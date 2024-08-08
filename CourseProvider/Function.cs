3325fa38f7292e51d1fb08dcfda666ae9a54f29c
Angelica Olsson <angelica.olsson1@learnet.se>
2024-08-08 13:12:12 +0200

    update

--- CourseProvider/Function.cs
+++ CourseProvider/Function.cs
@@ -2,6 +2,8 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.Azure.WebJobs;
 
 namespace CourseProvider
@@ -9,18 +11,22 @@ namespace CourseProvider
     public class Function
     {
         private readonly ILogger<Function> _logger;
+        private readonly BlobServiceClient _blobServiceClient;
 
-        public Function(ILogger<Function> logger)
+        public Function(ILogger<Function> logger, BlobServiceClient blobServiceClient)
         {
             _logger = logger;
+            _blobServiceClient = blobServiceClient;
         }
 
-        [Function(nameof(Function))]
-        public async Task Run([BlobTrigger("samples-workitems/{name}", Connection = "VaultUri")] Stream stream, string name)
+        [Function("ProcessBlob")]
+        public async Task Run(
+            [BlobTrigger("samples-workitems/{name}", Connection = "VaultUri")] Stream blobStream,
+            string name)
         {
-            using var blobStreamReader = new StreamReader(stream);
+            using var blobStreamReader = new StreamReader(blobStream);
             var content = await blobStreamReader.ReadToEndAsync();
-            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}");
+            _logger.LogInformation($"C# Blob function Processed blob\n Name: {name} \n Data: {content}");
         }
     }
 }
--- CourseProvider/Program.cs
+++ CourseProvider/Program.cs
@@ -8,24 +8,30 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Storage.Blobs;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices((context, services) =>
     {
+        // Add Application Insights telemetry
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
 
-        services.AddPooledDbContextFactory<DataContext>(x => x.UseSqlServer(context.Configuration.GetConnectionString("SqlServer")));
+        // Configure DbContext with SQL Server
+        services.AddPooledDbContextFactory<DataContext>(x =>
+            x.UseSqlServer(context.Configuration.GetConnectionString("SqlServer")));
 
+        // Register application services
         services.AddScoped<ICourseService, CourseService>();
 
+        // Configure GraphQL
         services.AddGraphQLFunction()
             .AddQueryType<CourseQuery>()
             .AddMutationType<CourseMutation>()
             .AddType<CourseType>();
 
-
+        // Configure CORS
         services.AddCors(options =>
         {
             options.AddDefaultPolicy(builder =>
@@ -36,14 +42,19 @@ var host = new HostBuilder()
             });
         });
 
+        // Register BlobServiceClient
+        services.AddSingleton(s =>
+        {
+            var connectionString = context.Configuration.GetConnectionString("VaultUri");
+            return new BlobServiceClient(connectionString);
+        });
 
+        // Ensure the database is created
         var sp = services.BuildServiceProvider();
         using var scope = sp.CreateScope();
         var dbContextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<DataContext>>();
         using var dbcontext = dbContextFactory.CreateDbContext();
         dbcontext.Database.EnsureCreated();
-
-
     })
     .Build();
 
